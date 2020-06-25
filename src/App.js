cedbc93892d559487f8cd929ae1aafeb64d7c7b4
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-06-25 15:34:03 +0530

    implement qrcode generation

--- src/App.js
+++ src/App.js
@@ -4,31 +4,108 @@ import "./App.css";
 
 function App() {
   const [generated, setGenerated] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [github, setGithub] = useState("");
+  const [result, setResult] = useState("");
 
   useEffect(() => {
+    setResult(`
+    ${firstName} ${lastName}
+    ${email}
+    https://github.com/${github}
+    `);
+  }, [firstName, lastName, email, github]);
+
+  const generate = () => {
     let qr = QRCode(0, "L");
-    qr.addData("x");
+    qr.addData(result);
     qr.make();
-    document.getElementById("placeHolder").innerHTML = qr.createImgTag();
-  }, [generated]);
+    document.getElementById("placeHolder").innerHTML = qr.createImgTag(5, 10);
+  };
 
   return (
-    <div className="container">
-      <div id="placeHolder"></div>
-      <form>
-        <input type="text" name="" id="" />
-        <input type="email" name="" id="" />
-        <input type="text" name="" id="" />
-        <div id="button-container">
-          <button type="reset" className="button">
-            Cancel
-          </button>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="container">
+        <div id="placeHolder"></div>
+
+        {generated && (
+          <div className="info">
+            <p>
+              {firstName} {lastName}
+            </p>
+            <p>{email}</p>
+            <p>https://github.com/{github}</p>
+          </div>
+        )}
+
+        {!generated && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setGenerated(true);
+              generate();
+            }}
+          >
+            <div className="name">
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              name="github"
+              id="github"
+              placeholder="Github Username"
+              value={github}
+              onChange={(e) => setGithub(e.target.value)}
+              required
+            />
+            <div id="button-container">
+              <button
+                type="reset"
+                onClick={() => {
+                  setFirstName("");
+                  setLastName("");
+                  setEmail("");
+                  setGithub("");
+                }}
+              >
+                Cancel
+              </button>
+              <button type="submit">Create</button>
+            </div>
+          </form>
+        )}
+      </div>
+      <button onClick={() => setGenerated(false)}>Edit</button>
+      <button className="print">Print</button>
+    </>
   );
 }
 
