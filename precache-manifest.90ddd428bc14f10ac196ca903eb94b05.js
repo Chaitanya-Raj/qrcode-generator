c575d401afe432cec21e3f675d69a9adaf790ea1
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-06-25 19:31:15 +0530

    Updates

---
+++ precache-manifest.90ddd428bc14f10ac196ca903eb94b05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607e52db5c8a096d71f41540d8f12a0d",
+    "url": "/qrcode-generator/index.html"
+  },
+  {
+    "revision": "888dadeddf0e65024384",
+    "url": "/qrcode-generator/static/css/main.f8b5d2e9.chunk.css"
+  },
+  {
+    "revision": "8008413bea7ccb51a69c",
+    "url": "/qrcode-generator/static/js/2.1bb497b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qrcode-generator/static/js/2.1bb497b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888dadeddf0e65024384",
+    "url": "/qrcode-generator/static/js/main.6a034848.chunk.js"
+  },
+  {
+    "revision": "4ac6ef5cac2218a088e3",
+    "url": "/qrcode-generator/static/js/runtime-main.edc96382.js"
+  },
+  {
+    "revision": "6d83b178608b35b82f9b71ad63db0656",
+    "url": "/qrcode-generator/static/media/background.6d83b178.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ecb8518a5f54da5abefd95db314d37a.js"
+  "/qrcode-generator/precache-manifest.90ddd428bc14f10ac196ca903eb94b05.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/qrcode-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
